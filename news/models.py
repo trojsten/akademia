5f54f9d249dc41d139dc60c930e328b4355d29ad
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-21 23:55:15 +0200

    Added a model for news entries and a corresponding ModelAdmin.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Entry(models.Model):
+    author = models.ForeignKey('auth.User', related_name='news_entries')
+    pub_date = models.DateTimeField(verbose_name='publication date', auto_now_add=True)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    slug = models.SlugField()
+
+    class Meta:
+        get_latest_by = 'pub_date'
+        ordering = ('-pub_date',)
+        verbose_name_plural = 'entries'
+
+    def __unicode__(self):
+        return self.title
--- news/urls.py
+++ news/urls.py
@@ -2,3 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
+urlpatterns = patterns('',
+)
