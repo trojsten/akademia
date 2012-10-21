a61ee75985930b7e2488a0ca357790b352259ecd
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-21 22:21:12 +0200

    Added South, updated requirements.

--- akademia/settings/__init__.py
+++ akademia/settings/__init__.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'social_auth',
     'ksp_login',
     # Uncomment the next line to enable the admin:
