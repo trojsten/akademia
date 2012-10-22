4bf7f63d0dc19bfc667e8e272049a37c593e524f
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-22 10:15:11 +0200

    Added a model for schools.

---
+++ events/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from events.models import School
+
+
+admin.site.register(School)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,22 @@
+# coding: utf-8
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    abbreviation = models.CharField(max_length=23,
+                                    verbose_name="skratka",
+                                    help_text="Sktatka názvu školy.")
+    verbose_name = models.CharField(max_length=47,
+                                    verbose_name="celý názov")
+    addr_name = models.CharField(max_length=47)
+    street = models.CharField(max_length=47)
+    city = models.CharField(max_length=47)
+    zip_code = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "škola"
+        verbose_name_plural = "školy"
+
+    def __unicode__(self):
+        return self.verbose_name
