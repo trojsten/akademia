248b91c6d7415f0b487fdbe78c4facb7db7c29b5
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-24 01:10:06 +0200

    Added models Event and Lecture.

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from events.models import School, AdditionalUserDetails
+from events.models import School, AdditionalUserDetails, Event, Lecture
 
 
 admin.site.register(School)
 admin.site.register(AdditionalUserDetails)
+admin.site.register(Event)
+admin.site.register(Lecture)
--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.sites.models import Site
 
 
 class School(models.Model):
@@ -32,5 +33,41 @@ class AdditionalUserDetails(models.Model):
                                'a&nbsp;pošlite nám e-mail.')
     is_teacher = models.BooleanField(verbose_name="som učiteľ")
 
+    class Meta:
+        verbose_name = "dodatočné údaje o užívateľoch"
+        verbose_name_plural = "dodatočné údaje o užívateľoch"
+
     def __unicode__(self):
         return "%s" % (self.user,)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length="47",
+                            help_text="Názov akcie, napr. Klub Trojstenu "
+                            "po Náboji FKS")
+    date = models.DateField()
+    deadline = models.DateTimeField()
+    sites = models.ManyToManyField(Site)
+
+    class Meta:
+        verbose_name = "akcia"
+        verbose_name_plural = "akcie"
+        ordering = ("-date",)
+
+    def __unicode__(self):
+        return "%s %s" % (self.name, self.date.year)
+
+
+class Lecture(models.Model):
+    event = models.ForeignKey(Event, verbose_name="akcia",
+                              related_name="lectures")
+    lecturer = models.CharField(max_length=47, verbose_name="prednášajúci")
+    title = models.CharField(max_length=147, verbose_name="názov prednášky")
+    abstract = models.TextField(verbose_name="abstrakt")
+    room = models.CharField(max_length=20, verbose_name="miestnosť")
+    time = models.TimeField(verbose_name="čas")
+
+    class Meta:
+        verbose_name = "prednáška"
+        verbose_name_plural = "prednášky"
+        ordering = ("event", "time")
