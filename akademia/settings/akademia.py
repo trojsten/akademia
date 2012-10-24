80d752a27f266f16a9b50ed50ababc10b411db39
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-25 01:50:21 +0200

    Fixed the import error in settings.

--- akademia/settings/akademia.py
+++ akademia/settings/akademia.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from akademia.settings.production import *
 
 SITE_ID = 1
--- akademia/settings/development.py
+++ akademia/settings/development.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from akademia.settings.common import *
 
 DATABASES = {
