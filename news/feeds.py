3c066b251c68eca3a910c3cd4353e2ad0bc663f4
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-25 11:46:05 +0200

    Made the news feed site-specific.

--- news/feeds.py
+++ news/feeds.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from django.contrib.syndication.views import Feed
+from django.contrib.sites.models import get_current_site
 from django.utils import feedgenerator
 
 from news.models import Entry
@@ -8,12 +9,16 @@ from news.models import Entry
 
 class NewsFeed(Feed):
     feed_type = feedgenerator.Atom1Feed
-    title = "Novinky Akadémie Trojstenu"
     link = "/"
-    subtitle = "Čo je nové ohľadom Akadémie a&nbsp;Klubu Trojstenu"
 
-    def items(self):
-        return Entry.objects.all()[:10]
+    def get_object(self, request):
+        return get_current_site(request)
+
+    def title(self, obj):
+        return "%s: Novinky" % (obj.name,)
+
+    def items(self, obj):
+        return Entry.objects.filter(sites__id__exact=obj.pk)[:10]
 
     def item_link(self):
         return "/"
