f50c25ce35e33307e0eaf544d176667133f7835f
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-30 13:10:00 +0100

    Top menu: active page highlighting

--- events/templatetags/events.py
+++ events/templatetags/events.py
@@ -10,3 +10,14 @@ register = template.Library()
 @register.assignment_tag(takes_context=True)
 def get_latest_event(context):
     return models.get_latest_event(context['request'])
+
+
+@register.simple_tag(takes_context=True)
+def active_page_class(context, url):
+    request = context['request']
+    path = request.path
+    if path.startswith('/news/') and url.startswith('/news'):
+        return "active"
+    if path == url:
+        return "active"
+    return ""
