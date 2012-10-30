5cef29fff91c6e058f2c56793d77d4e7c7b70556
Michal Petrucha <michal.petrucha@ksp.sk>
2012-10-30 10:13:00 +0100

    Better handling of additional user details in the admin.

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from events.models import School, AdditionalUserDetails, Event, Lecture
 
 
+class AdditionalUserDetailsInline(admin.StackedInline):
+    model = AdditionalUserDetails
+    can_delete = False
+
+
+class UserAdmin(UserAdmin):
+    inlines = (AdditionalUserDetailsInline, )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(School)
-admin.site.register(AdditionalUserDetails)
 admin.site.register(Event)
 admin.site.register(Lecture)
