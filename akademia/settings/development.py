75f0ceae599e86b83ed92decb3625fe30c30d25a
Michal Petrucha <michal.petrucha@ksp.sk>
2013-04-23 04:17:03 +0200

    Enabled django-debug-toolbar in dev settings.

--- akademia/settings/development.py
+++ akademia/settings/development.py
@@ -10,3 +10,9 @@ DATABASES = {
 
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+INSTALLED_APPS += ('debug_toolbar',)
+MIDDLEWARE_CLASSES = (('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    + MIDDLEWARE_CLASSES)
+
+INTERNAL_IPS = ('127.0.0.1',)
