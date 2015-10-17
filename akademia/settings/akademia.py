aaaf507140d7a116db1ecf00f3bec3c4e24d7dbd
Michal Petrucha <michal.petrucha@koniiiik.org>
2015-10-17 19:29:34 +0200

    Added ALLOWED_HOSTS to settings.

--- akademia/settings/akademia.py
+++ akademia/settings/akademia.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 from akademia.settings.production import *
 
 SITE_ID = 1
+ALLOWED_HOSTS = ['akademia.trojsten.sk']
--- akademia/settings/klub.py
+++ akademia/settings/klub.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 from akademia.settings.production import *
 
 SITE_ID = 2
+ALLOWED_HOSTS = ['klub.trojsten.sk']
